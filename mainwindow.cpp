a8475f130845123d74bef9fcd05d2633a4f9b37c
Wasgo097 <patrykpakula321@gmail.com>
2020-04-12 13:27:10 +0200

    Add gui layout and start developing communication

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,78 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+#include <QMessageBox>
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
-}
+    std::fstream str;
+    str.open("config");
+    if(str.good()){
+        try {
+            //qDebug()<<"Działa";
+            str>>ip>>port;
+            config_file=true;
+            //qDebug()<<ip.c_str()<<" "<<port.c_str();
+            str.close();
+            socket=std::make_unique<QTcpSocket>(this);
+            QString ip_temp=QString::fromStdString(ip);
+            quint16 port_temp=std::stoi(port);
+            socket->connectToHost(ip_temp,port_temp);
+            if(socket->waitForConnected()){
+                qDebug()<<"Polaczono";
+            }
+            else{
+                qDebug()<<"Niepolaczono";
+            }
+        }
+        catch (...) {
+            QMessageBox msg(this);
+            msg.setIcon(QMessageBox::Warning);
+            msg.setText("Błąd zawartości pliku konfiguracyjnego");
+            msg.setDetailedText("Skontaktuj się z twórcą oprogramowania");
+            msg.setWindowTitle("Błąd pliku konfiguracyjnego");
+            msg.setStandardButtons(QMessageBox::Ok);
+            msg.exec();
+        }
 
-MainWindow::~MainWindow()
-{
+    }
+    else{
+        QMessageBox msg(this);
+        msg.setIcon(QMessageBox::Warning);
+        msg.setText("Nie udało się otworzyć pliku konfiguracyjnego");
+        msg.setDetailedText("Skontaktuj się z twórcą oprogramowania");
+        msg.setWindowTitle("Błąd pliku konfiguracyjnego");
+        msg.setStandardButtons(QMessageBox::Ok);
+        msg.exec();
+    }
+}
+MainWindow::~MainWindow(){
     delete ui;
 }
+void MainWindow::on_btn_login_clicked(){
+    if(!config_file){
+        QMessageBox msg(this);
+        msg.setIcon(QMessageBox::Warning);
+        msg.setText("Nie udało się otworzyć pliku konfiguracyjnego i pobrać z niego danych");
+        msg.setDetailedText("Skontaktuj się z twórcą oprogramowania");
+        msg.setWindowTitle("Błąd pliku konfiguracyjnego");
+        msg.setStandardButtons(QMessageBox::Ok);
+        msg.exec();
+    }
+    else{
 
+    }
+}
+void MainWindow::on_btn_registration_clicked(){
+    if(!config_file){
+        QMessageBox msg(this);
+        msg.setIcon(QMessageBox::Warning);
+        msg.setText("Nie udało się otworzyć pliku konfiguracyjnego i pobrać z niego danych");
+        msg.setDetailedText("Skontaktuj się z twórcą oprogramowania");
+        msg.setWindowTitle("Błąd pliku konfiguracyjnego");
+        msg.setStandardButtons(QMessageBox::Ok);
+        msg.exec();
+    }
+    else{
+
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,21 +1,28 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
-
+#include <QTcpSocket>
+#include <QString>
+#include <memory>
+#include <fstream>
+#include <string>
+typedef  std::string string;
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow{
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+private slots:
+    void on_btn_login_clicked();
+    void on_btn_registration_clicked();
 private:
     Ui::MainWindow *ui;
+    std::unique_ptr<QTcpSocket> socket;
+    bool config_file=false;
+    string ip;
+    string port;
 };
 #endif // MAINWINDOW_H
