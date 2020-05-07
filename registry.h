e9afcde4b4719270db6850c4197a208b59b6bdae
Wasgo097 <patrykpakula321@gmail.com>
2020-05-07 22:41:28 +0200

    all types are valid

--- registry.h
+++ registry.h
@@ -18,21 +18,18 @@ public:
         str.open("logs.txt",std::fstream::in | std::fstream::out | std::fstream::app);
         if(str.good())
             qDebug()<<"good";
-        //exception.push_back(L"HKEY_USERS\\S-1-5-21-2315024851-3994538975-640974175-1001\\Software\\Classes\\VirtualStore");
+        exception.push_back(L"HKEY_USERS\\.DEFAULT\\Control Panel\\Desktop\\No");
     }
     ~Registry(){
         str.close();
     }
     BOOL TraverseRegistry(HKEY hKey, LPTSTR fullKeyName, LPTSTR subKey, LPBOOL flags){
-        //if(fullKeyName==L"HKEY_USERS\\S-1-5-21-2315024851-3994538975-640974175-1001\\Software\\Classes\\VirtualStore"){
-        //int size=wcslen(L"HKEY_USERS\\S-1-5-21-2315024851-3994538975-640974175-1001\\Software\\Classes\\VirtualStore");
         if(is_exception(fullKeyName)){
             qDebug()<<"Jestem tutaj";
             return true;
         }
         HKEY hSubKey;
         BOOL recursive = flags[0];
-        LONG result;
         DWORD valueType, index;
         DWORD numSubKeys, maxSubKeyLen, numValues, maxValueNameLen, maxValueLen;
         DWORD subKeyNameLen, valueNameLen, valueLen;
@@ -55,8 +52,8 @@ public:
 //            msg.setWindowTitle("Błąd dostępu do rejestru");
 //            msg.setStandardButtons(QMessageBox::Ok);
 //            msg.exec();
-            return 0;
-            //ReportError(_T("\nCannot open subkey"), 2, TRUE);
+            str<<"exc3 cannot open key"<<std::endl;
+            return false;
         }
         /*  Find max size info regarding the key and allocate storage */
         if (RegQueryInfoKey(hSubKey, NULL, NULL, NULL, &numSubKeys,&maxSubKeyLen,NULL,&numValues,&maxValueNameLen,&maxValueLen,NULL,&lastWriteTime) != ERROR_SUCCESS){
@@ -67,6 +64,7 @@ public:
 //            msg.setWindowTitle("Błąd dostępu do rejestru");
 //            msg.setStandardButtons(QMessageBox::Ok);
 //            msg.exec();
+            str<<"exc4 search info about key"<<std::endl;
             return 0;
         }
         subKeyName =(LPTSTR) malloc(TSIZE * (maxSubKeyLen + 1));   /* size in bytes */
@@ -79,8 +77,7 @@ public:
             valueNameLen = maxValueNameLen + 1; /* A very common bug is to forget to set */
             valueLen = maxValueLen + 1;     /* these values; both are in/out params  */
             swprintf_s(fullSubKeyName, _T("%s\\%s"), fullKeyName, subKeyName);
-            result = RegEnumValue(hSubKey, index, valueName, &valueNameLen, NULL, &valueType, value, &valueLen);
-            //if (result == ERROR_SUCCESS && GetLastError() == 0)
+            RegEnumValue(hSubKey, index, valueName, &valueNameLen, NULL, &valueType, value, &valueLen);
             WriteValue(valueName, valueType, value, valueLen,fullSubKeyName);
             /*  If you wanted to change a value, this would be the place to do it.
                 RegSetValueEx(hSubKey, valueName, 0, valueType, pNewValue, NewValueSize); */
@@ -88,18 +85,14 @@ public:
         //Second pass for subkeys
         for (index = 0; index < numSubKeys; index++) {
             subKeyNameLen = maxSubKeyLen + 1;
-            result = RegEnumKeyEx(hSubKey, index, subKeyName, &subKeyNameLen, NULL,NULL, NULL, &lastWriteTime);
-            //if (GetLastError() == 0) {
-            WriteSubKey(fullKeyName, subKeyName, &lastWriteTime, flags);
+            RegEnumKeyEx(hSubKey, index, subKeyName, &subKeyNameLen, NULL,NULL, NULL, &lastWriteTime);
+            //WriteSubKey(fullKeyName, subKeyName);
             /*  Display subkey components if -R is specified */
             if (recursive) {
-                //_stprintf(fullSubKeyName, _T("%s\\%s"), fullKeyName, subKeyName);
                 swprintf_s(fullSubKeyName, _T("%s\\%s"), fullKeyName, subKeyName);
                 TraverseRegistry(hSubKey, fullSubKeyName, subKeyName, flags);
             }
-            //}
         }
-        //_tprintf(_T("\n"));
         free(subKeyName);
         free(valueName);
         free(value);
@@ -109,48 +102,39 @@ public:
 private:
     BOOL WriteValue(LPTSTR valueName, DWORD valueType,	LPBYTE value, DWORD valueLen,LPTSTR fullKeyName){
         LPBYTE pV = value;
-        QString temp=QString::fromWCharArray(fullKeyName);
-        string tempp=temp.toStdString();
+        QString qtemp=QString::fromWCharArray(fullKeyName);
+        string stemp=qtemp.toStdString();
         unsigned long i;
-        int size=wcslen(valueName);
-        if(size==0){
-            str<<"exc"<<std::endl;
+        int namesize=wcslen(valueName);
+        if(namesize==0){
+            str<<"exc1 value without name"<<std::endl;
             return false;
         }
         string name="";
-        for(int i=0;i<size;i++)
+        for(int i=0;i<namesize;i++)
             name+=(char)valueName[i];
         //str<<temp.toStdString()<<std::endl;
-        str<<tempp<<std::endl;
+        str<<stemp<<std::endl;
         str<<name<<":"<<std::endl;
-        string valuee="val : ";
+        string svalue="val : ";
         std::stringstream sstream;
         switch (valueType) {
         case REG_FULL_RESOURCE_DESCRIPTOR: /* 9: Resource list in the hardware description */
         case REG_BINARY: /*  3: Binary data in any form. */
-            //window->add_to_output("binary");
-            //qDebug()<<"binary";
             str<<"bin "<<std::endl;
             for (i = 0; i < valueLen; i++, pV++){
-//                sprintf(temp,"%x",*pV);
-//                size=strlen(temp);
-//                for(j=0;j<size;j++)
-//                    valuee+=temp[i];
-//                  str<<std::hex<<(unsigned int)*pV<<" ";
                 sstream<<std::hex<<(unsigned int)*pV<<" ";
             }
-            sstream<<std::endl;
-            valuee+=sstream.str();
-            str<<valuee;
-            //str<<valuee<<std::endl;
-            //_tprintf(_T(" %x"), *pV);
-            //qDebug()<<*pV;
+            svalue+=sstream.str();
+            str<<svalue<<std::endl;
             break;
         case REG_DWORD: /* 4: A 32-bit number. */
             //_tprintf(_T("%x"), (DWORD)*value);
             //window->add_to_output("dword");
             //qDebug()<<"dword";
             str<<"dword "<<std::endl;
+            stemp=std::to_string((DWORD)*value);
+            str<<stemp<<std::endl<<std::endl;
             break;
         case REG_EXPAND_SZ: /* 2: null-terminated string with unexpanded references to environment variables (for example, “%PATH%”). */
         case REG_MULTI_SZ: /* 7: An array of null-terminated strings, terminated by two null characters. */
@@ -159,41 +143,29 @@ private:
             //window->add_to_output("sz");
             //qDebug()<<"str";
             str<<"str "<<std::endl;
+            qtemp=QString::fromWCharArray((LPTSTR)value);
+            stemp=qtemp.toStdString();
+            str<<stemp<<std::endl;
             break;
         case REG_DWORD_BIG_ENDIAN: /* 5:  A 32-bit number in big-endian format. */
         case REG_LINK: /* 6: A Unicode symbolic link. */
         case REG_NONE: /* 0: No defined value type. */
         case REG_RESOURCE_LIST: /* 8: A device-driver resource list. */
         default: //_tprintf(_T(" ** Cannot display value of type: %d. Exercise for reader\n"), valueType);
-            break;
+            str<<"exc2 unknow type of value"<<std::endl;
+            return false;
         }
         return TRUE;
     }
-    BOOL WriteSubKey(LPTSTR keyName, LPTSTR subKeyName, PFILETIME pLastWrite, LPBOOL flags)    {
-        BOOL longList = flags[1];
-        //SYSTEMTIME sysLastWrite;
+    BOOL WriteSubKey(LPTSTR keyName, LPTSTR subKeyName){
         QString temp=QString::fromWCharArray(keyName);
-        //_tprintf(_T("\n%s"), keyName);
         if (_tcslen(subKeyName) > 0){
             temp+="\\";
             temp+=QString::fromWCharArray(subKeyName);
-            //_tprintf(_T("\\%s "), subKeyName);
         }
-//        if(temp=="HKEY_USERS\\S-1-5-21-2315024851-3994538975-640974175-1001\\Software\\Classes\\VirtualStore\\MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Perflib\\009")
-//        {
-//            //
-//            qDebug()<<"proba "+ temp;
-//        }
         //qDebug()<<temp;
         //str<<temp.toStdString()<<std::endl;
         //window->add_to_output(temp);
-        if (longList) {
-//            FileTimeToSystemTime(pLastWrite, &sysLastWrite);
-//            _tprintf(_T("        %02d/%02d/%04d %02d:%02d:%02d"),
-//                sysLastWrite.wMonth, sysLastWrite.wDay,
-//                sysLastWrite.wYear, sysLastWrite.wHour,
-//                sysLastWrite.wMinute, sysLastWrite.wSecond);
-        }
         return TRUE;
     }
     bool is_exception(const wchar_t * str){
